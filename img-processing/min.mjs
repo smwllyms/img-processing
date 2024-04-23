c84d254b726cc646e6bf95f808cc8aae96b6de94
smwllyms <smwllyms@gmail.com>
2024-04-23 10:14:01 -0400

    Initial commit

---
+++ img-processing/min.mjs
@@ -0,0 +1,67 @@
+// v0.1
+
+export default (function() {
+
+    let scope = {};
+
+    scope.doEffect = function(rgb, kernel)
+    {
+
+        // Assume square kernel matrices
+        const kernelRowLen = Math.sqrt(kernel.length);
+
+        // Also assume there is a center (e.g., 3x3, 5x5)
+        const offset = Math.floor(kernelRowLen / 2);
+
+        // Traverse 
+        const width = rgb.length;
+        const height = rgb[0].length;
+
+        const newRGB = new Array(height);
+
+        for (let y = 0; y < height; y++) {
+
+            newRGB[y] = new Array(width);
+
+            for (let x = 0; x < height; x++) {
+
+                // Get the kernelRowLen x kernelRowLen around
+                let r = 0, g = 0, b = 0;
+                let pos = 0;
+
+                for (let ox = x - offset; ox <= x + offset; ox++)
+                {
+                    if (ox < 0 || ox >= width) {
+                        pos++;
+                        continue;
+                    }
+
+                    for (let oy = y - offset; oy <= y + offset; oy++)
+                    {
+                        if (oy < 0 || oy >= height) {
+                            pos++;
+                            continue;
+                        }
+
+                        const f = kernel[pos];
+                        r += rgb[oy][ox][0] * f;
+                        g += rgb[oy][ox][1] * f;
+                        b += rgb[oy][ox][2] * f;
+                        
+                        pos++;
+                    }
+                }
+
+                // sum += input[i + j] * kernel[kernel.length - 1 - j];
+                newRGB[y][x] = [r, g, b];
+            }
+        }
+
+        console.log(rgb)
+        console.log(newRGB)
+
+        return newRGB;
+    }
+    
+    return scope;
+})();
